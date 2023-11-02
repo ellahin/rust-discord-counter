26349e11a3253b051bdca4f184d99661db875ee0
ellahin <ella@ellahin.com>
2023-11-02 23:08:03 +1100

    add database create and migrate on load

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use sqlx::migrate::{MigrateDatabase, Migrator};
+use sqlx::SqlitePool;
+use std::env;
+use std::path::Path;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    dotenv().expect(".env file not found");
+
+    if env::var("DATABASE_URL").is_err() {
+        panic!("DATABASE_URL not in environment vars");
+    }
+
+    let database_url = env::var("DATABASE_URL").unwrap();
+
+    if !sqlx::Sqlite::database_exists(&database_url).await.unwrap() {
+        sqlx::Sqlite::create_database(&database_url).await.unwrap();
+    }
+
+    let migration_path = Path::new("./migrations");
+
+    let sql_pool = SqlitePool::connect(&database_url).await.unwrap();
+
+    Migrator::new(migration_path)
+        .await
+        .unwrap()
+        .run(&sql_pool)
+        .await
+        .unwrap();
 }
